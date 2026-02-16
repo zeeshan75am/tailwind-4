ea7e7e3dd11589fd404471c0ee345e60ee081760
zeeshan75am <zeeshan75am@gmail.com>
2026-02-17 04:25:00 +0500

    tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  darkMode:"class",
+  content: ["./src/**/*.{html,js}"], 
+  theme: {
+
+    extend: {
+      spacing:{
+        '48':'200px'
+      }
+
+    },
+  },
+  plugins: [],
+}
